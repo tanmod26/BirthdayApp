98189834b408342a854ee23280261c7a19aeb003
Tanisha <tanishamodi.mt@gmail.com>
2025-07-25 14:19:14 -0400

    SwiftData

--- BirthdayApp/ContentView.swift
+++ BirthdayApp/ContentView.swift
@@ -6,18 +6,17 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
-    @State private var friends: [Friend] = [
-        Friend(name: "Joan", birthday:.now),
-        Friend(name: "Sienna", birthday:.now)
-    ]
+    @Query private var friends: [Friend]
+    @Environment(\.modelContext) private var context
     @State private var newName = ""
     @State private var newBirthday = Date.now
     
     var body: some View {
         NavigationStack {
-            List(friends, id: \.name) { friend in
+            List(friends) { friend in
                 HStack {
                     Text(friend.name)
                     Spacer()
@@ -36,7 +35,7 @@ struct ContentView: View {
                     }
                     Button("Save") {
                         let newFriend = Friend(name: newName, birthday: newBirthday)
-                        friends.append(newFriend)
+                        context.insert(newFriend)
                         
                         
                         newName = ""
@@ -53,5 +52,6 @@ struct ContentView: View {
         
         #Preview {
             ContentView()
+                .modelContainer(for:Friend.self, inMemory: true)
         }
 
